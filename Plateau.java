181c46557d04822f189f7b3c2c981284f44bb514
Hadrien <hadriien@live.fr>
2017-05-09 09:21:21 +0200

    Ajout de la classe plateau

---
+++ Plateau.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Plateau extends JPanel {
+	private static int nbLignes = 9;
+	private static int nbColonnes = 9;
+
+	Plateau() {
+		super();
+		init();
+	}
+
+	public void init() {
+	}
+}
